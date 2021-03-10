0759175fc09664b4837fe5d45f0984198767f527
Joshua Blewitt <joshua.blewitt@protonmail.com>
2021-03-10 22:27:23 +0000

    Added; find, update, delete and better error handling. Plus Postman examples

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## Video Game API
 
-Welcome to the Video Game API! A free to use API that I created as a side project so I can practice Python and Flask. This API is publicly available and is hosted on Heroku. The project has it's own page (built with Flask), located here.
+Welcome to the Video Game API! A free to use RESTful API that I created as a side project so I can practice Python and Flask. This API is publicly available and is hosted on Heroku. The project has it's own page (built with Flask), located here.
 
 Feel free to clone this repo if you want to take a look at how this works!
 
@@ -21,7 +21,7 @@ This API uses JWT so you will need to authenticate yourself.
 
 ## Adding a game
 
-To add a game, you will need to access the 'addGame' endpoint (using a POST method) with a request in the following JSON structure:
+To add a game to the database, you will need to use the <strong>/addGame</strong> endpoint and submit a POST request. Example:
 ```
 {
     "name": "Super Mario 64",
@@ -33,18 +33,60 @@ To add a game, you will need to access the 'addGame' endpoint (using a POST meth
 ```
 
 ## Deleting a game
-To delete a game, you will need to access the 'game' endpoint (using a DELETE method) with the ID of the game you wish to delete. Example:
+To delete a game from the database, you will need to use the <strong>/deleteGame</strong> endpoint and submit a DELETE request. Example:
 ```
-http://127.0.0.1:5000/game/2
+{
+    "id": 3
+}
 ```
 
 ## Getting a game
-To find a specific game, you will need to access the 'game' endpoint (using a GET method) with the ID of the game you wish to find. Example:
+To find a game (or games), you will need to use the <strong>/findGame</strong> endpoint and submit a GET request. Example:
 ```
-http://127.0.0.1:5000/game/2
+{
+    "name": "64"
+}
 ```
 
 ## Displaying all games
+To display all games in the database, you will need to use the <strong>/displayGames</strong> endpoint. Submit a GET request to this endpoint and you will see a list of all the games in the database. Example response:
+```
+{
+    "Games": [
+        {
+            "Game": {
+                "Genre: ": "Fighting",
+                "ID: ": 1,
+                "Name: ": "X-Men Vs Street Fighter",
+                "Platform: ": "Sega Saturn",
+                "Publisher: ": "Capcom",
+                "Year": 1997
+            }
+        },
+        {
+            "Game": {
+                "Genre: ": "Platformer",
+                "ID: ": 2,
+                "Name: ": "Super Mario 64",
+                "Platform: ": "Nintendo 64",
+                "Publisher: ": "Nintendo",
+                "Year": 1996
+            }
+        },
+        {
+            "Game": {
+                "Genre: ": "Racing",
+                "ID: ": 3,
+                "Name: ": "Wave Race 64",
+                "Platform: ": "Nintendo 64",
+                "Publisher: ": "Nintendo",
+                "Year": 1996
+            }
+        }
+    ]
+}
+```
+
 
 ## Tech Stack
 The following was used for this application:
@@ -54,7 +96,7 @@ The following was used for this application:
     * [Flask framework](https://flask.palletsprojects.com/en/1.1.x/)
     * [Flask JWT Extended](https://pypi.org/project/Flask-JWT-Extended/)
     * Flask Restful
-    * SQL Alchemy
+    * [Flask SQL Alchemy](https://flask-sqlalchemy.palletsprojects.com/en/2.x/#)
     * os
 
 **Frontend**
--- app.py
+++ app.py
@@ -70,47 +70,156 @@ def showGames():
 def addGame():
     # Access the identity of the current user with get_jwt_identity
     current_user = get_jwt_identity()
-    #get fields from JSON request
-    name = request.json.get("name", None)
-    platform = request.json.get("platform", None)
-    publisher = request.json.get("publisher", None)
-    genre = request.json.get("genre", None)
-    year = request.json.get("year", None)
+    try:
+        #get fields from JSON request
+        name = request.json.get("name", None)
+        platform = request.json.get("platform", None)
+        publisher = request.json.get("publisher", None)
+        genre = request.json.get("genre", None)
+        year = request.json.get("year", None)
 
-    #use values from JSON to create new object
-    game = VideoGame(name=name, platform=platform, publisher=publisher, genre=genre, year=year)
+        if type(year) != int:
+            return {"You have entered an invalid year, please enter a number.": {"Value entered for year": year}}, 400
+    
+        if name == "" or platform == "" or publisher == "" or genre == "" or year == "" or name == None or platform == None or publisher == None or genre == None or year == None:
+            return {"Your request includes empty values, please try again": None}, 400
 
-    #add new object to DB
-    db.session.add(game)
-    db.session.commit()
+        #use values from JSON to create new object
+        game = VideoGame(name=name, platform=platform, publisher=publisher, genre=genre, year=year)
 
-    return game.json(), 201
+        #add new object to DB
+        db.session.add(game)
+        db.session.commit()
+
+        return {"The following game has been added":game.json()}, 201
+    except:
+        return {"Your request is invalid, are you using the correct format?": {"Format": {"name": "Game name", "platform": "Platform for game", "publisher": "Name of company", "genre": "Game genre", "year": 2000}}}, 400
 
 @app.route("/deleteGame", methods=["DELETE"])
 @jwt_required()
 def deleteGame():
-     # Access the identity of the current user with get_jwt_identity
-    current_user = get_jwt_identity()
-    id = request.json.get("id", None)
-    game = VideoGame.query.filter_by(id=id).first()
-    if game:
-        db.session.delete(game)
-        db.session.commit()
-        return {'The following has been deleted': game.json()}
-    else: 
-        {'No game found': None}, 404
+    try:
+        # Access the identity of the current user with get_jwt_identity
+        current_user = get_jwt_identity()
+        id = request.json.get("id", None)
+
+        if type(id) != int:
+            return {"You have entered an invalid id, please enter a number.": {"Value entered for id": id}}, 400
+
+        game = VideoGame.query.filter_by(id=id).first()
+        if game:
+            db.session.delete(game)
+            db.session.commit()
+            return {'The following has been deleted': game.json()}
+        else: 
+            return {'No game found': None}, 404
+    except:
+        return {"Your request is invalid, are you using the correct format?": {"Format": {"id": 1}}}, 400
+    
+
+@app.route("/findGame", methods=["GET"])
+@jwt_required()
+def findGame():
+    try:
+        # Access the identity of the current user with get_jwt_identity
+        current_user = get_jwt_identity()
+        name = request.json.get("name", None)
+        if name == None or name == "":
+            return {"Your request includes empty values, please try again": None}, 400
+        #creating search query with wild card each side
+        search = "%{}%".format(name)
+        games = VideoGame.query.filter(VideoGame.name.like(search)).all()
+        games_list = []
+        for game in games:
+                games_list.append(game.json())
+        if len(games_list) > 0:
+            return jsonify({"Games": games_list})
+        else:
+            return {"No games were found for the search term": name},404
+    except:
+        return {"Your request is invalid, are you using the correct format?": {"Format": {"name": "Game name"}}}, 400
+
+@app.route("/updateGame", methods=['PUT'])
+@jwt_required()
+def updateGame():
+    try:
+        # Access the identity of the current user with get_jwt_identity
+        current_user = get_jwt_identity()
+        id = request.json.get("id", None)
+        field = request.json.get("field", None)
+        value = request.json.get("value", None)
+
+        if id == None or field == None or field == "" or value == None or value == "":
+            return {"Your request includes empty values, please try again": None}, 400
+        
+        if type(id) != int:
+            return {"You have entered an invalid id, please enter a number.": {"Value entered for id": id}}, 400
+        
+        if field == "name":
+            game = VideoGame.query.get(id)
+            if game == None:
+                return {'No game found': None}, 404
+            game.name = value
+            db.session.add(game)
+            db.session.commit()
+            return {"Game updated:": game.json(), "Value updated": field}, 200
+        
+        elif field == "platform":
+            game = VideoGame.query.get(id)
+            if game == None:
+                return {'No game found': None}, 404
+            game.platform = value
+            db.session.add(game)
+            db.session.commit()
+            return {"Game updated:": game.json(), "Value updated": field}, 200
+
+        elif field == "publisher":
+            game = VideoGame.query.get(id)
+            if game == None:
+                return {'No game found': None}, 404
+            game.publisher = value
+            db.session.add(game)
+            db.session.commit()
+            return {"Game updated:": game.json(), "Value updated": field}, 200
+
+        elif field == "genre":
+            game = VideoGame.query.get(id)
+            if game == None:
+                return {'No game found': None}, 404
+            game.genre = value
+            db.session.add(game)
+            db.session.commit()
+            return {"Game updated:": game.json(), "Value updated": field}, 200
+        
+        elif field == "year":
+            game = VideoGame.query.get(id)
+            if game == None:
+                return {'No game found': None}, 404
+            game.year = value
+            db.session.add(game)
+            db.session.commit()
+            return {"Game updated:": game.json(), "Value updated": field}, 200
+        
+        else:
+            return {"Invalid field entered": field}, 400
+    except:
+        return {"Your request is invalid, are you using the correct format?": {"Format": {"id": 1, "field": "name", "value": "Super Mario 64"}}}, 400
+
 
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username", None)
-    password = request.json.get("password", None)
+    try:
+        username = request.json.get("username", None)
+        password = request.json.get("password", None)
 
-    user = authenticate(username=username, password=password)
-    if user == None:
-        return jsonify({"msg": "Bad username or password"}), 401
+        user = authenticate(username=username, password=password)
+        if user == None:
+            return jsonify({"Login failed": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=username)
-    return jsonify(access_token=access_token)
+        access_token = create_access_token(identity=username)
+        return jsonify(access_token=access_token)
+    except:
+        return {"Your request is invalid, are you using the correct format?": {"Format": {"username": "username", "password": "password"}}}, 400
 
 
 @app.route('/')
